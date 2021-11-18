5bc2b2d434b463c95879fd9ca2f32a9264899f7a
Vitaliy Pytel <pytel.vitaliy@gmail.com>
2021-11-18 19:13:20 +0200

    added pagination logic

--- src/main/java/com/example/application/views/treeGrid/PdfCreator.java
+++ src/main/java/com/example/application/views/treeGrid/PdfCreator.java
@@ -1,14 +1,10 @@
 package com.example.application.views.treeGrid;
 
-import com.vaadin.flow.data.provider.hierarchy.HierarchicalQuery;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDDocumentInformation;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
-import org.apache.pdfbox.pdmodel.font.PDType0Font;
-import org.apache.pdfbox.pdmodel.font.PDType1CFont;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
-import org.apache.pdfbox.pdmodel.font.PDType3Font;
 
 import java.io.File;
 import java.io.IOException;
@@ -20,46 +16,26 @@ import java.util.List;
 import static java.util.stream.Collectors.*;
 
 public class PdfCreator {
-    private final int LINES_COUNT = 20;
+    private final int LINES_COUNT = 40;
 
     public void createPdf() {
-        File ROOT = new File("/home");
-//        File ROOT = new File("src/main/resources/file-storage/");
+        File ROOT = new File("src/main/resources/file-storage/");
         List<File> fileNamesList = getFileList();
-        //creating empty pdf document
         PDDocument document = new PDDocument();
-        PDPage pdPage = new PDPage();
-        document.addPage(pdPage);
 
         PDDocumentInformation documentInformation = document.getDocumentInformation();
         documentInformation.setAuthor("Vitaliy Pytel");
         documentInformation.setTitle("Test document");
 
         try {
-            PDPageContentStream contentStream = new PDPageContentStream(document, pdPage);
-            //  -->>    start text
-            contentStream.beginText();
-            contentStream.newLineAtOffset(25f, 725f);
-            contentStream.setLeading(14.5f);
-            contentStream.setFont(PDType1Font.HELVETICA, 12f);
-            contentStream.setCharacterSpacing(3f);
-            int recorded = 0;
-            for (File file : fileNamesList) {
-                contentStream.showText(getStringToDoc(file));
-                contentStream.newLine();
-                recorded++;
-                if (recorded == LINES_COUNT) {
-                    document.addPage(new PDPage());
-                }
+            var listP = listOfPages(fileNamesList, LINES_COUNT);
+            for (List<File> fileList : listP) {
+                fileList.stream()
+                                .forEach(System.out::println);
+                System.out.println("-------------------------");
+                writePage(fileList, document);
             }
-            contentStream.endText();
-            //  -->>    end text
-            contentStream.close();
-
-            fileNamesList.stream()
-                    .forEach(System.out::println);
             document.save(new File("src/main/resources/file-storage/PDF/formattedFileListText.pdf"));
-            // closing
             document.close();
         } catch (IOException e) {
             e.printStackTrace();
@@ -70,7 +46,7 @@ public class PdfCreator {
     private List<File> getFileList() {
         List<File> fileNamesList = new ArrayList<>();
         try {
-            fileNamesList = Files.walk(Path.of("/home/vitaliy/Util_Progs"))
+            fileNamesList = Files.walk(Path.of("/home/vitaliy/Util_Progs/Discord"))
                     .map(Path::toFile)
                     .collect(toList());
         } catch (IOException e) {
@@ -80,6 +56,42 @@ public class PdfCreator {
     }
 
     private String getStringToDoc(File file) {
-        return " ".repeat(file.getParent().split("/").length) + file.getName();
+        return file.isFile() ? " ".repeat(file.getParent().split("/").length) + file.getName()
+                : "--" + file.getName();
+    }
+
+    private void writePage(List<File> list, PDDocument document) throws IOException {
+        PDPage page = new PDPage();
+
+        document.addPage(page);
+        PDPageContentStream contentStream = new PDPageContentStream(document, page);
+        //  -->>    start text
+        contentStream.beginText();
+        contentStream.newLineAtOffset(25f, 725f);
+        contentStream.setLeading(14.5f);
+        contentStream.setFont(PDType1Font.HELVETICA, 12f);
+        contentStream.setCharacterSpacing(3f);
+
+        for (File file : list) {
+            contentStream.showText(getStringToDoc(file));
+            contentStream.newLine();
+        }
+        contentStream.endText();
+        contentStream.close();
+    }
+
+    private List<List<File>> listOfPages(List<File> fileList, int lines) {
+        List<List<File>> pagedList = new ArrayList<>();
+        int count = 0;
+        List<File> internal = new ArrayList<>();
+        for (File file : fileList) {
+            internal.add(file);
+            count++;
+            if (count % 20 == 0) {
+                pagedList.add(internal);
+                internal = new ArrayList<>();
+            }
+        }
+        return pagedList;
     }
 }
